0b276331366587be85cfea4d3e9c92da84de742b
Kim Doy <heydoyhey@gmail.com>
2020-07-16 21:25:36 +0900

    Update t2.c

--- C/exer1/t2.c
+++ C/exer1/t2.c
@@ -1,8 +1,6 @@
 /***************************
- * Student Number : 1815364
+ * 두개의 정수를 입력받아 사칙연산 계산 결과를 출력
  * Name : Kim Doy
- ---------------------------
- * 실습 1 : exer1/t2.c
  * created on Tue 31 Mar 2020
  ***************************/
 
@@ -10,7 +8,7 @@
 #include <stdio.h>
 
 int main(void){
-	//두개의 정수를 입력받아 사칙연산 계산 결과를 출력
+	
 	int a, b;
 	printf("Enter two integers : ");
 	scanf("%d %d", &a, &b);
